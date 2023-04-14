ae6bec48423e3b3f820e48edfc354ce2d4e91f37
Tom Hackshaw <tom@tomhackshaw.com>
2023-04-14 23:19:47 +1200

    Rounded containers

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
             <p className="text-xl text-gray-700 mb-8" data-aos="fade-up" data-aos-delay="200">Instant 3D experiences for anyone.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn rounded-md text-white bg-teal-500 hover:bg-teal-800 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+                <a className="btn rounded-md text-white bg-teal-500 hover:bg-teal-800 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start creating</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn rounded-md text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
